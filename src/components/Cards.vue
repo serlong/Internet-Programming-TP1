7030afb363b2a66d4b2fc7ec3d8709f94eae5084
serlong <128038147+serlong@users.noreply.github.com>
2023-11-03 19:25:47 +0700

    Update

---
+++ src/components/Cards.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="Cards-Box" :style="{ backgroundColor: bg }">
+    <div>
+      <div class="Text">{{ Text }}</div>
+      <div class="btn">
+        <Buttons :bgBtn="bgBtn" :TextBtn="TextBtn" />
+      </div>
+    </div>
+    <img class="Img" :src="Img" alt="Item" />
+  </div>
+</template>
+
+<script>
+import Buttons from "./Buttons.vue";
+export default {
+  name: "Cards",
+  props: {
+    Img: String,
+    Text: String,
+    bg: String,
+    bgBtn: String,
+    TextBtn: String,
+  },
+  components: {
+    Buttons,
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@1,300;1,400;1,500;1,600;1,700&family=Poppins:wght@300;400;500;600;800&family=Quicksand:wght@400;600;700&display=swap");
+
+.Cards-Box {
+  width: 482px;
+  height: 300px;
+  border: 1px solid rgba(129, 177, 61, 1);
+  box-shadow: 2px 2px 5px lightgray;
+  border-radius: 10px;
+  margin-top: 20px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.Img {
+  margin-left: 80px;
+  width: 400px;
+}
+.Text {
+  font-family: "Quicksand", sans-serif;
+  font-size: 22px;
+  width: 240px;
+  font-weight: 600;
+  position: absolute;
+  top: 80px;
+  left: 40px;
+}
+.btn {
+  position: absolute;
+  bottom: 40px;
+  left: 40px;
+}
+</style>
---
+++ src/views/CardsPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="Cards-Row">
+    <Cards
+      v-for="i in Cards"
+      :key="i.Cards"
+      :bg="i.bg"
+      :Img="i.Img"
+      :Text="i.Text"
+      :bgBtn="i.btncolor"
+      :TextBtn="i.TextBtn" />
+  </div>
+</template>
+
+<script>
+import Cards from "../components/Cards.vue";
+import Buttons from "../components/Buttons.vue";
+import Img1 from "../assets/images/berry bottle.png";
+import Img2 from "../assets/images/onion.png";
+import Img3 from "../assets/images/veg.png";
+export default {
+  name: "CardsProduct",
+  components: {
+    Cards,
+    Buttons,
+  },
+  data() {
+    return {
+      Cards: [
+        {
+          Text: "Everyday Fresh & Clean with Our Products",
+          Img: Img2,
+          btncolor: "rgba(59, 183, 126, 1)",
+          TextBtn: "Shop Now",
+        },
+        {
+          Text: "Make your Breakfast Healthy and Easy",
+          Img: Img1,
+          bg: "rgba(243, 232, 232, 1)",
+          btncolor: "rgba(205, 50, 17, 1)",
+          TextBtn: "Shop Now",
+        },
+        {
+          Text: "The Best Organic Products Online",
+          Img: Img3,
+          bg: "rgba(231, 234, 243, 1)",
+          btncolor: "rgba(253, 192, 64, 1)",
+          TextBtn: "Shop Now",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@1,300;1,400;1,500;1,600;1,700&family=Poppins:wght@300;400;500;600;800&family=Quicksand:wght@400;600;700&display=swap");
+
+.Cards-Row {
+  width: 100%;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 15px;
+}
+</style>
